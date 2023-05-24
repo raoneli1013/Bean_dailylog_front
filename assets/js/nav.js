50dd6988d5fda1d21c00ae7f34fd553cf106f2df
sojin park <psjin977@naver.com>
2023-05-24 22:13:17 +0900

    FEAT profile ing

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,6 +1,49 @@
 $(document).ready(function () {
-
+    // 상단 네비 html을 붙여주는 함수
     $("#navbar-container").load("index_nav.html");
 
 
+
+    //유저 토큰을 가져와서 로그인시 닉네임 표시
+    const payload = localStorage.getItem("payload")
+
+    const payload_parse = JSON.parse(payload)
+    console.log(payload)
+
+    // const intro = document.getElementById("intro")
+    // intro.innerText = `${payload_parse.nickname}님 안녕하세요`
+
+    // let navbarRight = document.getElementById("navbar-right")
+    // let newLi = document.createElement("li")
+    // newLi.setAttribute("class", "nav-item")
+
+    // let logoutBtn = document.createElement("button")
+    // logoutBtn.setAttribute("class", "nav-link btn")
+    // logoutBtn.innerText = "로그아웃"
+    // logoutBtn.setAttribute("onclick", "handleLogout()")
+
+    // newLi.appendChild(logoutBtn)
+
+    // navbarRight.appendChild(newLi)
+
+    // let loginbtn = document.getElementById("login-btn")
+    // loginbtn.style.display = "none";
+
+    // let signupbtn = document.getElementById("signup-btn")
+    // signupbtn.style.display = "none";
+
+
 });
+
+
+//유저 로그아웃
+async function handleLogout() {
+    event.preventDefault();
+    localStorage.removeItem("access")
+    localStorage.removeItem("refresh")
+    localStorage.removeItem("payload")
+    location.replace('index.html')
+}
+async function handlecreatearticle() {
+    location.href = 'create_article.html'
+}
--- assets/js/profile.js
+++ assets/js/profile.js
@@ -1,10 +1,10 @@
-window.onload = () => async function () {
+$(document).ready(async function () {
     console.log("로딩됨");
 
     $('.hidden').hide();
-    // $('.permission').hide();
-    // $('.permission2').hide();
-    // $('.withdrawal').hide();
+    $('.permission').hide();
+    $('.permission2').hide();
+    $('.withdrawal').hide();
 
     let urlParam = new URLSearchParams(window.location.search);
     let user_id = urlParam.get('user_id');
@@ -12,4 +12,39 @@ window.onload = () => async function () {
 
     const response = await getProfile(user_id);
     console.log(response)
-}
+
+    if (localStorage.getItem("payload")) {
+
+        const payload = localStorage.getItem("payload");
+        const payload_parse = JSON.parse(payload)
+
+        const token = localStorage.getItem("access");
+        const me_id = payload_parse.user_id;
+        console.log(me_id)
+        const email = payload_parse.email;
+
+        if (me_id == user_id) {
+            $('.permission').show();
+            console.log("내가 이 ")
+        } else {
+            // 다른 사용자 라면
+            $('.permission2').show();
+            $('#unfollow-btn').hide();
+
+            response.followers.includes(account)
+            const isAccountInArray = response.followers.includes(account);
+
+            if (isAccountInArray) {
+                // 팔로우가 되어 있다면
+                $('#follow-btn').hide(); // '팔로우하기'를 숨김.
+                $('#following-btn').show(); // '팔로잉'을 보여줌.
+            }
+            else {
+                // 팔로우가 되어 있지 않다면
+                $('#follow-btn').show(); // '팔로우하기'를 보여줌.
+                $('#following-btn').hide(); // '팔로잉'을 숨김.
+            }
+        }
+
+    }
+})
