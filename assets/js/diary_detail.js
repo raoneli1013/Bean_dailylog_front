8f1b5a13105e6e53c83692232628fc54115ec903
imjanny <wogns0808@gmail.com>
2023-05-26 16:49:43 +0900

    FEAT: diary CRUD

--- assets/js/diary_detail.js
+++ assets/js/diary_detail.js
@@ -32,11 +32,23 @@ window.onload = async function getDiaryDetail() {
   const createdDate = document.getElementById('created_date')
   const updatedDate = document.getElementById('updated_date')
 
-
+  // 본문컨텐츠 추가
   title.innerText = response_json['title']
   content.innerText = response_json['content']
   createdDate.innerText = new Date().toDateString(response_json['created_date']) + " 작성"
   updatedDate.innerText = new Date().toDateString(response_json['updated_date']) + " 수정"
+
+  // 수정 및 삭제 버튼 보여주기
+const editDiaryButton = document.getElementById('edit-diary-button');
+const deleteDiaryButton = document.getElementById('delete-diary-button');
+
+if (currentUser === response_json['user']) {
+  editDiaryButton.style.display = 'block';
+  deleteDiaryButton.style.display = 'block';
+} else {
+  editDiaryButton.style.display = 'none';
+  deleteDiaryButton.style.display = 'none';
+}
 
   // user 정보
   const response_user = await fetch(`${backend_base_url}` + '/user/' + response_json['user'] + '/', {
@@ -45,6 +57,8 @@ window.onload = async function getDiaryDetail() {
   author = await response_user.json()
   const nickname = document.getElementById('nickname')
   nickname.innerText = "닉네임 : " + author['nickname']
+  console.log("??",response_json)
+  
 
 
   //key값에 image가 들어왔는지 확인
@@ -61,7 +75,7 @@ window.onload = async function getDiaryDetail() {
     const imageBox = document.getElementById('image-box');
     const feedImage = document.createElement("img")
     feedImage.setAttribute('class', 'imagecard')
-    // feedImage.setAttribute("src", `${backend_base_url}` + `${response_json['article_img']}`)
+    feedImage.setAttribute("src", `${backend_base_url}` + `${response_json['article_img']}`)
     imageBox.appendChild(feedImage)
 
   }
@@ -78,7 +92,7 @@ window.onload = async function getDiaryDetail() {
   console.log("comment", response_json_comment)
 
   const diaryComment = document.getElementById("diary_comment");
-
+  // 코멘트 찾아서 붙여주기
   response_json_comment.forEach(comment => {
     const commentContainer = document.createElement('div');
     commentContainer.setAttribute('class', 'comment-container');
@@ -131,6 +145,8 @@ window.onload = async function getDiaryDetail() {
 
     diaryComment.appendChild(editForm);
 
+    
+
     // console.log("start", response_json)
     // 좋아요 카운트
     $('#likes_count').text(response_json.likes_count);
@@ -139,7 +155,7 @@ window.onload = async function getDiaryDetail() {
 
 }
 
-
+// 댓글 입력 함수
 async function inputComment() {
   const comment_content = document.getElementById("comment-content").value
   const response_input = await fetch(`${backend_base_url}/diary/comment/${diary_id}/`, {
@@ -158,19 +174,23 @@ async function inputComment() {
 
 let editingCommentId = null;
 
-// 수정하기 버튼클릭
+// 댓글 수정하기 버튼클릭
 function editComment(id) {
-  editingCommentId = id;
+  const commentContent = document.getElementById(`comment-content-${id}`);
+  const editForm = document.getElementById(`edit-form-${id}`);
+  const editInput = document.getElementById(`edit-input-${id}`);
 
-  document.getElementById('edit-comment-form').style.display = 'block'; // input 창을 보여줌
-  document.getElementById('edit-comment-content').value = '';
+  // 댓글 내용을 인풋창으로 변경
+  editInput.value = commentContent.innerText;
+  commentContent.style.display = 'none';
+  editForm.style.display = 'block';
 }
 
-// 수정된 내용 저장
-async function saveEditedComment() {
-  const newContent = document.getElementById('edit-comment-content').value;
+// 수정된 댓글 내용 저장
+async function saveEditedComment(id) {
+  const newContent = document.getElementById(`edit-input-${id}`).value;
 
-  const response = await fetch(`${backend_base_url}/diary/comment/${diary_id}/${editingCommentId}/`, {
+  const response = await fetch(`${backend_base_url}/diary/comment/${diary_id}/${id}/`, {
     headers: {
       'Authorization': 'Bearer ' + localStorage.getItem('access'),
       'Content-Type': 'application/json',
@@ -185,10 +205,15 @@ async function saveEditedComment() {
 }
 
 // 수정 취소
-function cancelEdit() {
-  // 입력 폼을 숨깁니다.
-  document.getElementById('edit-comment-form').style.display = 'none';
-  editingCommentId = null;
+function cancelEdit(id) {
+  const commentContent = document.getElementById(`comment-content-${id}`);
+  const editForm = document.getElementById(`edit-form-${id}`);
+  const editInput = document.getElementById(`edit-input-${id}`);
+
+  // 인풋창을 숨기고 원래의 댓글 내용을 보여줌
+  commentContent.style.display = 'block';
+  editForm.style.display = 'none';
+  editInput.value = '';
 }
 
 // 댓글 삭제
@@ -240,5 +265,71 @@ async function ClickBookmark() {
 
 }
 
+//다이어리 수정 
+async function editDiary() {
+  const editButton = document.getElementById('edit-diary-button');
+  const title = document.getElementById('title');
+  const content = document.getElementById('content');
+
+  if (editButton.innerText === '수정') {
+    // 수정 모드로 변경
+    editButton.innerText = '저장';
+
+    const titleInput = document.createElement('input');
+    titleInput.setAttribute('type', 'text');
+    titleInput.setAttribute('id', 'updated-title');
+    titleInput.value = title.innerText;
+    title.innerText = '';
+    title.appendChild(titleInput);
+
+    const contentInput = document.createElement('textarea');
+    contentInput.setAttribute('id', 'updated-content');
+    contentInput.value = content.innerText;
+    content.innerText = '';
+    content.appendChild(contentInput);
+  } else {
+    // 저장 모드로 변경
+    const updatedTitle = document.getElementById('updated-title').value;
+    const updatedContent = document.getElementById('updated-content').value;
+
+    const response = await fetch(`${backend_base_url}/diary/${diary_id}/`, {
+      headers: {
+        'Authorization': 'Bearer ' + localStorage.getItem('access'),
+        'Content-Type': 'application/json',
+      },
+      method: 'PUT',
+      body: JSON.stringify({
+        title: updatedTitle,
+        content: updatedContent,
+      }),
+    });
+
+    if (response.status === 200) {
+      // 수정된 내용을 받아와서 디테일 페이지를 다시 로드
+      window.location.href = `${frontend_base_url}/diary_detail.html?id=${diary_id}`;
+    } else {
+      alert('게시글 수정에 실패했습니다.');
+    }
+  }
+}
 
 
+// 게시글 삭제 버튼 클릭 시
+async function deleteDiary() {
+  const confirmDelete = confirm("게시글을 삭제하시겠습니까?");
+  if (confirmDelete) {
+    const response = await fetch(`${backend_base_url}/diary/${diary_id}/`, {
+      headers: {
+        'Authorization': 'Bearer ' + localStorage.getItem('access'),
+      },
+      method: 'DELETE',
+    });
+
+    if (response.status === 204) { // 삭제 완료 204 나오면 삭제
+      alert("게시글이 삭제되었습니다.");
+      window.location.href = `${frontend_base_url}/feed.html`; // 피드페이지로 돌려보냄
+    } else {
+      alert("게시글 삭제에 실패했습니다.");
+    }
+  }
+}
