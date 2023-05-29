8206644e6bb63645b5ada266bf01dc193da7221e
sojin park <psjin977@naver.com>
2023-05-29 10:06:58 +0900

    Fix:detail

--- assets/js/diary_detail.js
+++ assets/js/diary_detail.js
@@ -279,56 +279,6 @@ likeButtonClick.addEventListener('click', async function () {
 
 
 
-// //좋아요 -> diary_api.js로 이동
-
-// export async function LikeLike() {
-//   const response = await fetch(`${backend_base_url}/diary/${diary_id}/likes/`, {
-//     headers: {
-//       "Authorization": "Bearer " + localStorage.getItem("access"),
-//       'content-type': 'application/json',
-//     },
-//     method: 'POST',
-//   })
-
-//   response_json = await response.json()
-//   console.log("start", response_json)
-//   const like_image = response_json.likes_image
-//   console.log(like_image)
-
-
-//   if (response.status === 200) {
-//     alert("하튜")
-//     if (like_image === true) {
-//     } else if (like_image === false) {
-
-//     }
-//     location.reload();
-//   }
-// }
-
-// // 준열님 코드 참고해서 다시작성
-// // 로그인이 안되어서 제대로 실행되는지 알 수 없는 상태라 약간 안전빵 느낌으로 
-// // 준열님 코드를 클론코딩해보았습니다.. -소진
-// export async function ClickLike(diary_id) {
-//   const url = `${BACK_BASE_URL}/diary/${diary_id}/likes/`;
-//   try {
-//     const response = await fetch(url, {
-//       method: "POST",
-//       headers: {
-//         Authorization: `Bearer` + localStorage.getItem("access"),
-//         'content-type': 'application/json',
-//       },
-//     });
-
-//     if (!response.ok) {
-//       throw new Error("Request failed");
-//     }
-
-//     console.log("Request succeeded");
-//   } catch (error) {
-//     console.error("Request failed:", error.message);
-//   }
-// }
 
 
 
