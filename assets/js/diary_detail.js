8b9e3eb11784d86a755db03c02c8e6a5cf5edf22
sojin park <psjin977@naver.com>
2023-05-29 10:02:31 +0900

    detail 수정

--- assets/js/diary_detail.js
+++ assets/js/diary_detail.js
@@ -281,30 +281,30 @@ likeButtonClick.addEventListener('click', async function () {
 
 // //좋아요 -> diary_api.js로 이동
 
-export async function LikeLike() {
-  const response = await fetch(`${backend_base_url}/diary/${diary_id}/likes/`, {
-    headers: {
-      "Authorization": "Bearer " + localStorage.getItem("access"),
-      'content-type': 'application/json',
-    },
-    method: 'POST',
-  })
+// export async function LikeLike() {
+//   const response = await fetch(`${backend_base_url}/diary/${diary_id}/likes/`, {
+//     headers: {
+//       "Authorization": "Bearer " + localStorage.getItem("access"),
+//       'content-type': 'application/json',
+//     },
+//     method: 'POST',
+//   })
 
-  response_json = await response.json()
-  console.log("start", response_json)
-  const like_image = response_json.likes_image
-  console.log(like_image)
+//   response_json = await response.json()
+//   console.log("start", response_json)
+//   const like_image = response_json.likes_image
+//   console.log(like_image)
 
 
-  if (response.status === 200) {
-    alert("하튜")
-    if (like_image === true) {
-    } else if (like_image === false) {
+//   if (response.status === 200) {
+//     alert("하튜")
+//     if (like_image === true) {
+//     } else if (like_image === false) {
 
-    }
-    location.reload();
-  }
-}
+//     }
+//     location.reload();
+//   }
+// }
 
 // // 준열님 코드 참고해서 다시작성
 // // 로그인이 안되어서 제대로 실행되는지 알 수 없는 상태라 약간 안전빵 느낌으로 
